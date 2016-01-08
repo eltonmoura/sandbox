ac4ff715e0a1b5c391cabda492f18425d705a8c5
Elton Moura <elton.ms@gmail.com>
2016-01-08 04:15:34 -0200

    .

--- get_images.php
+++ get_images.php
@@ -3,46 +3,73 @@ $data_path = "/usr/local/data/comics";
 $link_pattern = "#<div\sclass=\"img\-url\">//(.*?)</div>#is";
 $base_url = "https://";
 
-if (!isset($argv[1])) { //|| !is_file($argv[1]
+if (!isset($argv[1])) {
     die("Uso: " . $argv[0] . " url|file\n");
 }
 
 $content = file_get_contents($argv[1]);
 
-if (preg_match_all($link_pattern, $content, $matches)) {
-    #print_r($matches[1]);
-    foreach ($matches[1] as $link) {
-        if (!preg_match("#/([-_0-9a-zA-Z]+)/([-_0-9a-zA-Z]+\.jpg)#", $link, $m)) {
-            die("Não foi possível gerar o nome do arquivo de destino em '$link'\n");
-        }
+if (!preg_match_all($link_pattern, $content, $matches)) {
+    die("Não foram encontradas imagens\n");
+}
 
-        $gallery = $m[1];
-        $dest_dir = $data_path . "/" . $gallery;
-        if (!is_dir($dest_dir)) {
-            mkdir($dest_dir);
-        }
+print("Foram encontradas " . count($matches[1]) . " imagens\n");
 
-        $dest_file = $dest_dir . "/" . $m[2];
+if (preg_match("#<title>(.*?)\|.*?</title>#is", $content, $m)) {
+    $dest_dir = $data_path . "/" . asSlug($m[1]);
+    if (!is_dir($dest_dir)) {
+        mkdir($dest_dir);
+    }
+}
 
-        if (is_file($dest_file)) {
-            continue;
-        }
-
-        $url = $base_url . $link;
-        print("Copiando $url para $dest_file.\n");
-        copy($url, $dest_file);
+$i = 0;
+foreach ($matches[1] as $link) {
+    $i++;
+    if (!preg_match("#/([-_0-9a-zA-Z]+)/([-\._0-9a-zA-Z]+\.\w{3})#", $link, $m)) {
+        die("Não foi possível gerar o nome do arquivo de destino em '$link'\n");
     }
 
-    $zipFile = $data_path . "/" . $gallery . ".cbz";
+    $dest_file = $dest_dir . "/" . $m[2];
+    if (is_file($dest_file)) {
+        continue;
+    }
+
+    $url = $base_url . $link;
+    print("Copiando $url (" . $i .")\n");
+    copy($url, $dest_file);
+}
+
+makeComicBookFromDir($dest_dir);
+
+print("Done.\n");
+
+function asSlug($text)
+{
+    $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+    $text = trim($text, '-');
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    $text = strtolower($text);
+    $text = preg_replace('~[^-\w]+~', '', $text);
+    if (empty($text)) {
+        return 'n-a';
+    }
+    return $text;
+}
+
+function makeComicBookFromDir($dir)
+{
+    if (!is_dir($dir)) {
+        return false;
+    }
+    $zipFile = $dir . ".cbz";
     $zipArchive = new ZipArchive();
     if (!$zipArchive->open($zipFile, ZIPARCHIVE::OVERWRITE)) {
         die("Failed to create archive\n");
     }
-    $zipArchive->addGlob($dest_dir . "/*.jpg");
+    $zipArchive->addGlob($dir . "/*");
     if (!$zipArchive->status == ZIPARCHIVE::ER_OK) {
         echo "Failed to write files to zip\n";
     }
     $zipArchive->close();
-
+    return true;
 }
-print("Done.\n");
